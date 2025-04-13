7659da6c83455c796900d1a960586b8a513a2861
shivtcs1983 <shivtcs1983@gmail.com>
2025-04-12 23:00:01 -0400

    v2

--- src/main-responsive-layout/MainResponsiveLayout.tsx
+++ src/main-responsive-layout/MainResponsiveLayout.tsx
@@ -1,24 +1,13 @@
 
 import "./MainResponsiveLayout.scss"
-import ComplexHeader from '../header/ComplexHeader';
-import ExpandCollapseControl from '../components/ExpandCollapseControl';
-import CustomSlider from '../components/CustomSlider';
-import CustomMarquee from '../components/CustomMarquee';
-import ContactInfo from '../contact-us/ContactInfo';
-import LeafletMap from '../leafletmap/LeafletMap';
-import HeroSlider from '../hero-slider/HeroSlider';
 import NasdaqOptions from './../nasdaq/NasdaqOptions';
-import FormData from './../nasdaq/FormData';
-import DataTable from './../nasdaq/DataTable';
-import NASDAQStock from './../nasdaq/NASDAQStock';
-import FinnhubOptions from './../nasdaq/FinnhubOptions';
+
 const MainResponsiveLayout = () => {
   return (
     <div className="application-level">
 
       <div className="main-container11">
-        {/* <DataTable/> */}
-        {/* <FormData/> */}
+          {/* <FormData/> */}
         {/* <NASDAQStock/> */}
         {/* <FinnhubOptions/> */}
         <NasdaqOptions />
