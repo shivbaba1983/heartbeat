9fc1b66ae11caf341a8eb991ddbb961e7f980824
shivtcs1983 <shivtcs1983@gmail.com>
2025-05-09 06:52:59 -0400

    marquee-update

--- src/components/PriceMarquee.tsx
+++ src/components/PriceMarquee.tsx
@@ -1,13 +1,29 @@
 import Marquee from "react-fast-marquee";
 
+// const PriceMarquee = ({ lastPrice , selectedTicker}) => {
+//   return (
+//     <div className="bg-black text-white p-2">
+//       <Marquee gradient={false} speed={50}>
+//         <span className="mx-4">{selectedTicker} {lastPrice}</span>
+//       </Marquee>
+//     </div>
+//   );
+// };
+
+// export default PriceMarquee;
+
+import React from "react";
+import "./PriceMarquee.scss";
+
 const PriceMarquee = ({ lastPrice , selectedTicker}) => {
   return (
-    <div className="bg-black text-white p-2">
-      <Marquee gradient={false} speed={50}>
+    <div className="sticky-marquee">
+      <div className="marquee-content">
         <span className="mx-4">{selectedTicker} {lastPrice}</span>
-      </Marquee>
+      </div>
     </div>
   );
 };
 
 export default PriceMarquee;
+
