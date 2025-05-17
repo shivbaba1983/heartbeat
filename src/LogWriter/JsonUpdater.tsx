c792a463aa968396f264f29e77f74741eed42336
shivtcs1983 <shivtcs1983@gmail.com>
2025-05-17 08:55:29 -0400

    ticker-correction

--- src/LogWriter/JsonUpdater.tsx
+++ src/LogWriter/JsonUpdater.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { NASDAQ_TOKEN, ETF_List, LogTickerList, JSON_UPDATE_TIME, IS_AWS_API, tickerListData, volumeOrOpenInterest, dayOrMonthData } from '../constant/HeartbeatConstants';
-import { isWithinMarketHours } from '../common/nasdaq.common';
+import { isWithinMarketHours, getFridayOfCurrentWeek, getTodayInEST, getEffectiveDate, getComingFriday } from './../common/nasdaq.common';
 import { writeS3JsonFile, writeToS3Bucket } from '../services/WriteS3Service';
+import { getNasdaqOptionData } from './../services/NasdaqDataService';
 
 const JsonUpdater = () => {
     const [data, setData] = useState([]);
@@ -12,8 +13,8 @@ const JsonUpdater = () => {
         const fetchMyData = async () => {
             const interval = setInterval(() => {
                 LogTickerList.forEach(ticker => {
-                   // if (isWithinMarketHours()) {
-                        postDataToS3Bucket(ticker); // write data to json file in s3 bucket    
+                    // if (isWithinMarketHours()) {
+                    postDataToS3Bucket(ticker); // write data to json file in s3 bucket    
                     // } else {
                     //     console.log('⏸ Market is closed. Skipping API call.');
                     // }
@@ -22,8 +23,8 @@ const JsonUpdater = () => {
 
             return () => clearInterval(interval); // Cleanup on unmount
         };
-       // if (isWithinMarketHours()) {
-            fetchMyData();
+        // if (isWithinMarketHours()) {
+        fetchMyData();
         // }
         // else {
         //     console.log('⏸ Market is closed. Skipping API call.');
@@ -34,16 +35,43 @@ const JsonUpdater = () => {
         setData([]);
         let assetclass = 'stocks';
         const selectedDayOrMonth = 'day';
-        try {
 
+        try {
+            let lstPrice;
+            let rows = [];
+            let selectedDate = ''
             if (ETF_List.includes(ticker))
                 assetclass = 'ETF'
-            const res = await axios.get(`${NASDAQ_TOKEN}/api/options/${ticker}/${assetclass}/${selectedDayOrMonth}`);
-            // const res = await axios.get(`http://localhost:5000/api/options/${selectedTicker}/${assetclass}/${selectedDayOrMonth}`);
-            const rows = res.data?.data?.table?.rows || [];
-            let lstPrice = res.data?.data?.lastTrade;
-            const match = lstPrice ? lstPrice.match(/\$([\d.]+)/) : 0;
-            lstPrice = match ? parseFloat(match[1]) : 0;
+            if (selectedDayOrMonth === 'day' && (assetclass === 'ETF')) {
+                if (["TQQQ", "SOXL", "TSLL", "SQQQ"].includes(ticker))
+                    selectedDate = getComingFriday();
+                else
+                    selectedDate = getEffectiveDate();
+            }
+            else if (selectedDayOrMonth === 'day' && assetclass === 'stocks') {
+                selectedDate = getComingFriday();
+            }
+
+            if (IS_AWS_API) {
+                const response = await getNasdaqOptionData(ticker, assetclass, selectedDayOrMonth, selectedDate);
+                const latestData = await response.json();
+                lstPrice = latestData?.data?.lastTrade;
+                const match = lstPrice ? lstPrice.match(/\$([\d.]+)/) : 0;
+                lstPrice = match ? parseFloat(match[1]) : 0;
+                rows = latestData?.data?.table?.rows || [];
+            } else {
+                //***********to call local api end point*************
+                const tempToken = import.meta.env.VITE_STOCK_API_URL;
+                const res = await axios.get(`${tempToken}/api/options/${ticker}/${assetclass}/${selectedDayOrMonth}`);
+                rows = res.data?.data?.table?.rows || [];
+                lstPrice = res.data?.data?.lastTrade;
+                const match = lstPrice.match(/\$\d+(\.\d+)?/);
+                lstPrice = match ? match[0] : null;
+            }
+            // rows = res.data?.data?.table?.rows || [];
+            //  lstPrice = res.data?.data?.lastTrade;
+            // const match = lstPrice ? lstPrice.match(/\$([\d.]+)/) : 0;
+            // lstPrice = match ? parseFloat(match[1]) : 0;
             const total = await caculateSum(rows);
             if (total.c_Volume > 0) {
                 if (IS_AWS_API) {
@@ -57,7 +85,7 @@ const JsonUpdater = () => {
 
 
         } catch (err) {
-            console.error('Failed to get options data log writer-JsonUpdater:', err);
+            console.error(`Failed to get options data log writer-JsonUpdater: ${ticker}`, err);
         }
     };
 
