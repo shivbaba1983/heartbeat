88f01d010c34ecf286fda817c98d8849a8302223
shivtcs1983 <shivtcs1983@gmail.com>
2025-10-05 22:43:09 -0400

    quartely-dashboard-corrcetion

--- src/analytics/QuarterlyOpenInterest/QuarterlyDashboard.tsx
+++ src/analytics/QuarterlyOpenInterest/QuarterlyDashboard.tsx
@@ -1,16 +1,37 @@
+import React, { useState } from 'react';
 import { TWOQuarterlyOpenInterest } from './TWOQuarterlyOpenInterest';
-import { DynamicTickerOptionTable } from './DynamicTickerOptionTable'
-const QuarterlyDashboard = () => {
+import { DynamicTickerOptionTable } from './DynamicTickerOptionTable';
+import './QuarterlyDashboard.scss';
 
-    return (
-        <div>
-            <p>Welcome, This Analytics Dashboard page</p>
-            <div>
-                <DynamicTickerOptionTable />
-                <TWOQuarterlyOpenInterest />
-            </div>
+const QuarterlyDashboard = () => {
+  const [showTwoQuarterly, setShowTwoQuarterly] = useState(false);
+
+  return (
+    <div className="quarterly-dashboard-container">
+      <p className="dashboard-header">Welcome, This is the Analytics Dashboard page</p>
+
+      <div className="section">
+        <DynamicTickerOptionTable />
+      </div>
+
+      <div className="checkbox-section">
+        <label className="toggle-label">
+          <input
+            type="checkbox"
+            checked={showTwoQuarterly}
+            onChange={(e) => setShowTwoQuarterly(e.target.checked)}
+          />
+          &nbsp; Display TWO Quarterly Open Interest
+        </label>
+      </div>
+
+      {showTwoQuarterly && (
+        <div className="section">
+          <TWOQuarterlyOpenInterest />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default QuarterlyDashboard;
